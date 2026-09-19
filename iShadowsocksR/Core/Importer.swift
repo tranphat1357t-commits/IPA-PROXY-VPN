089e82e1de197d30204ccca8be75cceb65a7cc26
tranphat1357t-commits <tranphat1357t@gmail.com>
2026-09-19 09:46:30 +0700

    Add files via upload

---
+++ iShadowsocksR/Core/Importer.swift
@@ -0,0 +1,158 @@
+//
+//  Importer.swift
+//
+//  Created by LEI on 4/15/16.
+//  Copyright © 2016 TouchingApp. All rights reserved.
+//
+
+import Foundation
+import Async
+import PotatsoModel
+import PotatsoLibrary
+import QRScanner
+
+struct Importer {
+    
+    weak var viewController: UIViewController?
+    var completion: ((Bool) -> Void)?
+    
+    init(vc: UIViewController, completion: ((Bool) -> Void)? = nil) {
+        self.viewController = vc
+        self.completion = completion
+    }
+    
+    func importConfigFromUrl() {
+        var urlTextField: UITextField?
+        let alert = UIAlertController(title: "Import Config From URL".localized(), message: nil, preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Input URL".localized()
+            urlTextField = textField
+        }
+        alert.addAction(UIAlertAction(title: "OK".localized(), style: .default, handler: { (action) in
+            if let input = urlTextField?.text {
+                self.onImportInput(input)
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel".localized(), style: .cancel, handler: nil))
+        viewController?.present(alert, animated: true, completion: nil)
+    }
+    
+    func importProxyNodesFromSubscriptionUrl() {
+        var urlTextField: UITextField?
+        let alert = UIAlertController(title: "Import nodes from subscription URL".localized(), message: nil, preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Input subscription URL".localized()
+            urlTextField = textField
+        }
+        alert.addAction(UIAlertAction(title: "OK".localized(), style: .default, handler: { (action) in
+            if let input = urlTextField?.text {
+                self.importSubscription(input)
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel".localized(), style: .cancel, handler: nil))
+        viewController?.present(alert, animated: true, completion: nil)
+    }
+    
+    func importConfigFromQRCode() {
+        let parent = self.viewController?.navigationController
+        let scanner = QRScannerController()
+        scanner.success = { code in
+            parent?.dismiss(animated: true, completion: nil)
+            if code != nil {
+                self.onImportInput(code!)
+            }
+        }
+        parent?.present(scanner, animated: true, completion: nil)
+    }
+    
+    func onImportInput(_ result: String) {
+        if ProxyNode.uriIsShadowsocks(result) {
+            importSS(result)
+        }else {
+            NSLog("Not support anymore")
+        }
+    }
+    
+    func importSS(_ source: String) {
+        do {
+            let defaultName = "___scanresult"
+            let proxyNode = try ProxyNode(dictionary: ["name": defaultName as AnyObject, "uri": source as AnyObject])
+            var urlTextField: UITextField?
+            let alert = UIAlertController(title: "Add a new proxy".localized(), message: "Please set name for the new proxy".localized(), preferredStyle: .alert)
+            alert.addTextField { (textField) in
+                textField.placeholder = "Input name".localized()
+                textField.text = proxyNode.name
+                urlTextField = textField
+            }
+            alert.addAction(UIAlertAction(title: "OK".localized(), style: .default){ (action) in
+                guard let text = urlTextField?.text?.trimmingCharacters(in: CharacterSet.whitespaces) else {
+                    self.onConfigSaveCallback(false, error: "Name can't be empty".localized())
+                    return
+                }
+                proxyNode.name = text
+                do {
+                    try proxyNode.validate()
+                    try DBUtils.add(proxyNode)
+                    self.onConfigSaveCallback(true, error: nil)
+                }catch {
+                    self.onConfigSaveCallback(false, error: error)
+                }
+            })
+            alert.addAction(UIAlertAction(title: "Cancel".localized(), style: .cancel) { action in
+            })
+            viewController?.present(alert, animated: true, completion: nil)
+        }catch {
+            self.onConfigSaveCallback(false, error: error)
+        }
+        if let vc = viewController {
+            Alert.show(vc, message: "Fail to parse proxy config".localized())
+        }
+    }
+    
+    func onConfigSaveCallback(_ success: Bool, error: Error?) {
+        Async.main(after: 0.5) {
+            self.viewController?.hideHUD()
+            if !success {
+                var errorDesc = ""
+                if let error = error {
+                    errorDesc = "(\(error))"
+                }
+                if let vc = self.viewController {
+                    Alert.show(vc, message: "\("Fail to save config.".localized()) \(errorDesc)")
+                }
+            }else {
+                self.viewController?.showTextHUD("Import Success".localized(), dismissAfterDelay: 1.5)
+            }
+            self.completion?(success)
+        }
+    }
+
+    func verifyUrl (_ urlString: String?) -> Bool {
+        if let urlString = urlString {
+            if let url = NSURL(string: urlString) {
+                return UIApplication.shared.canOpenURL(url as URL)
+            }
+        }
+        return false
+    }
+
+    func importSubscription(_ result: String) {
+        if verifyUrl(result) {
+            let randomInt = Int.random(in: 0..<1000)
+            
+            let proxyNode = ProxyNode()
+            proxyNode.type = .Subscription
+            proxyNode.host = result
+            proxyNode.name = "Subscribe-" + String(format: "%04d", randomInt)
+            proxyNode.port = 8341
+            
+            do {
+                try proxyNode.validate()
+                try DBUtils.add(proxyNode)
+                self.onConfigSaveCallback(true, error: nil)
+            } catch {
+                self.onConfigSaveCallback(false, error: error)
+            }
+        }
+    }
+}
---
+++ iShadowsocksR/Core/VPN.swift
@@ -0,0 +1,37 @@
+//
+//  ProxyService.swift
+//
+//  Created by LEI on 12/28/15.
+//  Copyright © 2015 TouchingApp. All rights reserved.
+//
+
+import Foundation
+import Async
+import PotatsoModel
+import PotatsoLibrary
+
+class VPN {
+    
+    static func switchVPN(_ group: ConfigurationGroup, completion: ((Error?) -> Void)? = nil) {
+        let defaultUUID = Manager.sharedManager.defaultConfigGroup.uuid
+        let isDefault = defaultUUID == group.uuid
+        if !isDefault {
+            Manager.sharedManager.stopVPN()
+            Async.main(after: 1) {
+                _switchDefaultVPN(group, completion: completion)
+            }
+        }else {
+            _switchDefaultVPN(group, completion: completion)
+        }
+    }
+
+    fileprivate static func _switchDefaultVPN(_ group: ConfigurationGroup, completion: ((Error?) -> Void)? = nil) {
+        Manager.sharedManager.setDefaultConfigGroup(group.uuid, name: group.name)
+        Manager.sharedManager.switchVPN { (manager, error) in
+            Async.main{
+                completion?(error)
+            }
+        }
+    }
+    
+}
