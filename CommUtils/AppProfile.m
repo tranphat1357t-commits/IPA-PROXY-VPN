ce6497afb0f66ee159623214a8047b57d5485217
tranphat1357t-commits <tranphat1357t@gmail.com>
2026-09-19 09:05:05 +0700

    Add files via upload

---
+++ CommUtils/AppProfile.m
@@ -0,0 +1,59 @@
+//
+//  PotatsoManager.m
+//
+//  Created by LEI on 4/4/16.
+//  Copyright © 2016 TouchingApp. All rights reserved.
+//
+
+#import "AppProfile.h"
+
+@implementation AppProfile
+
++ (NSString *) sharedGroupIdentifier {
+    // Try to avoid hardcoding Group IDs into the source code.
+    // This is a fragile implementation that can be easily broken.
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSURL *url = bundle.bundleURL;
+    if ([url.pathExtension isEqualToString:@"appex"]) {
+        // Peel off two directory levels - MY_APP.app/PlugIns/MY_APP_EXTENSION.appex
+        url = [[url URLByDeletingLastPathComponent] URLByDeletingLastPathComponent];
+        bundle = [NSBundle bundleWithURL:url];
+    }
+    return [@"group." stringByAppendingString:[bundle bundleIdentifier]];
+}
+
++ (NSURL *)sharedUrl {
+    NSString *groupID = [self sharedGroupIdentifier];
+    return [[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:groupID];
+}
+
++ (NSURL *)sharedDatabaseUrl {
+    return [[self sharedUrl] URLByAppendingPathComponent:@"potatso.realm"];
+}
+
++ (NSUserDefaults *)sharedUserDefaults {
+    NSString *groupID = [self sharedGroupIdentifier];
+    return [[NSUserDefaults alloc] initWithSuiteName:groupID];
+}
+
++ (NSURL * _Nonnull)sharedGeneralConfUrl {
+    return [[self sharedUrl] URLByAppendingPathComponent:@"general.xxx"];
+}
+
++ (NSURL *)sharedSocksConfUrl {
+    return [[self sharedUrl] URLByAppendingPathComponent:@"socks.xxx"];
+}
+
++ (NSURL *)sharedProxyConfUrl {
+    return [[self sharedUrl] URLByAppendingPathComponent:@"proxy.xxx"];
+}
+
++ (NSURL *)sharedHttpProxyConfUrl {
+    return [[self sharedUrl] URLByAppendingPathComponent:@"http.xxx"];
+}
+
++ (NSURL * _Nonnull)sharedLogUrl {
+    return [[self sharedUrl] URLByAppendingPathComponent:@"tunnel.log"];
+}
+
+@end
